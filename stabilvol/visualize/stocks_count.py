e6b119492ed803b8d491d7917125bf60b815ed1e
Federico Amato <federico.amato1998@gmail.com>
2022-12-18 10:17:18 +0100

    Add crash-centered window in plots

--- stabilvol/visualize/stocks_count.py
+++ stabilvol/visualize/stocks_count.py
@@ -4,14 +4,18 @@ Plot stocks count sliding the window
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import Colormap
-from stabilvol.utility.definitions import ROOT, MARKETS
+from stabilvol.utility.definitions import ROOT, MARKETS, FIRST_VALID_DAY, LAST_VALID_DAY
 
 
 class StocksCount:
     def __init__(self):
-        self.data = pd.read_pickle(ROOT / 'data/external/stocks_counts.pickle')
+        self.data: pd.DataFrame = pd.read_pickle(ROOT / 'data/external/stocks_counts_5dstartend.pickle')
         self.windows_lengths = self.data.columns.unique(level="Window Length")
         self.n_windows = len(self.windows_lengths)
+
+    @property
+    def max_values(self):
+        return self.data.max()
 
     def plot_counts_in_window(self, rows=None, cols=None):
         if rows is None:
@@ -26,9 +30,15 @@ class StocksCount:
                                          colormap="Accent",
                                          xlabel=""
                                          )
-            ax.axvline(pd.Timestamp('2008-09-15')-pd.Timedelta(int(self.windows_lengths[i])*365/2, unit='D'),
-                       ls='--', c='r')
+            # Add two vertical lines indicating the window containing the WS crash in the middle
+            crash_day = pd.Timestamp('2008-09-15')
+            left_end = crash_day - pd.Timedelta(int(self.windows_lengths[i])*365/2, unit='D')
+            right_end = crash_day + pd.Timedelta(int(self.windows_lengths[i]) * 365 / 2, unit='D')
+            ax.axvline(crash_day, ls='--', c='r')
+            ax.axvspan(left_end, right_end, alpha=0.1)
+            ax.text(crash_day+pd.Timedelta('200d'), 1000, "Crash-centered\nwindow", ha='left', size=9, color='red', alpha=0.5)
             ax.legend(loc="upper center", bbox_to_anchor=(0.5, -0.1), ncol=5)
+            ax.set_xlim(pd.Timestamp(FIRST_VALID_DAY), pd.Timestamp(LAST_VALID_DAY))
         fig.tight_layout(h_pad=1)
         plt.show()
 
