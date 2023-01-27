d5b80669e94457d98f048746c7938d90cedaa865
Federico Amato <federico.amato1998@gmail.com>
2023-01-27 12:16:02 +0100

    Add FHT counting scripts

--- stabilvol/analyze/count_fht.py
+++ stabilvol/analyze/count_fht.py
@@ -2,32 +2,63 @@
 
 Count FHT
 """
-import pandas as pd
+import click
 import matplotlib.pyplot as plt
-from utility.classes.data_extraction import DataExtractor
-from utility.classes.stability_analysis import StabilVolter
-from utility.definitions import ROOT
+import pandas as pd
 import time
 
+try:
+    from stabilvol.utility.classes.data_extraction import DataExtractor
+    from stabilvol.utility.classes.stability_analysis import StabilVolter
+except ModuleNotFoundError:
+    print('Not found')
+    from ..utility.classes.data_extraction import DataExtractor
+    from ..utility.classes.stability_analysis import StabilVolter
 
-def state_of_x(x, start, end):
-    if x > start:
-        return 1
-    elif x < end:
-        return -1
-    else:
-        return 0
+from utility.definitions import ROOT, MARKETS, FIRST_VALID_DAY, LAST_VALID_DAY
+
+
+def main(markets, start, end, duration, nbins):
+    for market in markets:
+        # The duration is used by default to extract data
+        if duration is None:
+            # Take the duration in years from start to end
+            duration = len(pd.date_range(start=start, end=end, freq='Y'))
+        accountant = DataExtractor(start_date=start, duration=duration)
+        data = accountant.extract_data(ROOT / f'data/interim/{market}.pickle')
+        accountant.plot_selection()
+        analyst = StabilVolter()
+        stabilvol = analyst.get_stabilvol(data)
+        mfht = analyst.get_average_stabilvol(nbins=nbins)
+        # Save
+        analyst.save_fht(market)
+        analyst.save_mfht(market)
+        # Plot
+        analyst.plot_fht()
+        ax = analyst.plot_mfht(edit=True)
+        ax.set_xlim(0, 0.15)
+        plt.show()
+    return None
+
+
+@click.command()
+@click.option('-m', '--markets', multiple=True, default=MARKETS)
+@click.option('-s', '--start', type=str, default=FIRST_VALID_DAY)
+@click.option('-e', '--end', type=str, default=LAST_VALID_DAY)
+@click.option('-d', '--duration', type=int, default=None)
+@click.option('-n', '--nbins', type=int, default=2000)
+def count_fht(markets, start, end, duration, nbins):
+    main(markets, start, end, duration, nbins)
+    return None
 
 
 if __name__ == "__main__":
-    start_time = time.time()
-    accountant = DataExtractor(start_date='2002-01-01', duration=10)
-    data = accountant.extract_data(ROOT / 'data/interim/GF.pickle')
-    for end_level in [-1.0, ]:
-        analyst = StabilVolter(end_level=end_level)
-        analyst.get_stabilvol(data.iloc[:100, 0:1].reset_index(drop=True))
-        plot_ax = analyst._plot_states()
-        plt.show()
-        plot_count_ax = analyst._plot_count_states()
-        plt.show()
-    print(f"\n\n***** First Execution time: {time.time() - start_time} ***** ")
+    markets = MARKETS
+    start_date = '2002-01-01'
+    end_date = None
+    duration = 12
+    nbins = 3000
+    start_time = time.time()  # to track execution time
+    main(markets, start_date, end_date, duration, nbins)
+    end_time = time.time()
+    print(f"\n\n***** Execution time for {len(markets)} markets: {end_time - start_time}s ***** ")
--- stabilvol/cli.py
+++ stabilvol/cli.py
@@ -6,6 +6,7 @@ sys.path.extend(['G:\\UNIPA\\ECONOFISICA\\stabilizing_volatility'])
 
 import click
 from data.count_stocks import count_stocks
+from analyze.count_fht import count_fht
 
 
 @click.group()
@@ -13,6 +14,7 @@ def cli():
     return None
 
 cli.add_command(count_stocks)
+cli.add_command(count_fht)
 
 
 if __name__ == "__main__":
