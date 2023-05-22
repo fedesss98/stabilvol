a793e8d8cb7619df112cd6e28e164bf727ed8696
Federico Amato <federico.amato1998@gmail.com>
2023-05-22 11:48:48 +0200

    Time total stabilvol

--- stabilvol/single_stabilvol.py
+++ stabilvol/single_stabilvol.py
@@ -5,21 +5,20 @@ from utility.classes.data_extraction import DataExtractor
 from utility.classes.stability_analysis import StabilVolter, MeanFirstHittingTimes
 
 import matplotlib.pyplot as plt
-from time import time
 from pathlib import Path
 
 DATABASE = Path('../data/interim')
 
 MARKET = 'UN'
-START_DATE = '1990-01-01'
-END_DATE = '2022'
+START_DATE = '2010-01-01'
+END_DATE = '2022-07-01'
 CRITERION = 'percentage'
-VALUE = 0.8
+VALUE = 0.05
 
 START_LEVEL = -0.1
 END_LEVEL = -1.5
 TAU_MAX = 1e6
-NBINS = 6000
+NBINS = 2000
 
 
 def print_indicators_table(header, indicators):
@@ -38,7 +37,21 @@ def print_indicators_table(header, indicators):
     print(f"{'-' * table_width}")
 
 
+def get_stabilvol(accountant, analyst):
+    data = accountant.extract_data(DATABASE / f'{MARKET}.pickle')
+
+    analysis_info = {
+        'Market': MARKET,
+        # 'Start date': accountant.start_date.strftime("%Y-%m-%d"),
+        # 'Window length': int(accountant.window.length.days / 365.2425)
+    }
+    stabilvol = analyst.get_stabilvol(data, 'multi', **analysis_info)
+    return stabilvol
+
+
 if __name__ == '__main__':
+    from datetime import datetime
+
     accountant = DataExtractor(
         start_date=START_DATE,
         end_date=END_DATE,
@@ -46,28 +59,25 @@ if __name__ == '__main__':
         criterion_value=VALUE,
         sigma_range=(1e-5, 1e5)
     )
-    data = accountant.extract_data(DATABASE / f'{MARKET}.pickle')
-
     analyst = StabilVolter(
         start_level=START_LEVEL,
         end_level=END_LEVEL,
         tau_max=TAU_MAX)
-    analysis_info = {
-        'Market': MARKET,
-        'Start date': accountant.start_date.strftime("%Y-%m-%d"),
-        'Window length': int(accountant.window.length.days / 365.2425)
-    }
-    start_time = time()
-    stabilvol = analyst.get_stabilvol(data, 'multi', **analysis_info)
-    end_time = time()
+
+    # GET STABILVOL
+    start_time = datetime.now()
+    stabilvol = get_stabilvol(accountant, analyst)
+    end_time = datetime.now()
     print(f"\n\nStabilvol calculated in {end_time-start_time} seconds\n\n")
-    indicators = analyst.get_indicators(stabilvol)
-    print_indicators_table('FHT Indicators'.upper(), indicators)
+
+    # STATISTICS
+    print_indicators_table('FHT Indicators'.upper(), analyst.get_indicators(stabilvol))
     analyst.plot_fht()
     plt.show()
-    mfht = MeanFirstHittingTimes(stabilvol, nbins=1000, max_volatility=0.2)
+
+    # GET MFHT
+    mfht = MeanFirstHittingTimes(stabilvol, nbins=NBINS, max_volatility=0.2)
+    # STATISTICS
     baricenters = mfht.baricenters
-    mfht_indicators = mfht.indicators
-    print_indicators_table('MFHT Indicators'.upper(), mfht_indicators)
+    print_indicators_table('MFHT Indicators'.upper(), mfht.indicators)
     mfht.plot()
-    mfht.plot(invisible=True)
